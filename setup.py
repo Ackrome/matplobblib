9879f7ec0714df7df079673a2db511518dc23550
Ackrome <ivantishchenko59@gmail.com>
2024-10-26 22:13:11 +0300

    commit

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='matplobblib',
-    version='0.1.27',
+    version='0.1.28',
     packages=find_packages(),
     description='Just a library for some subjects',
     author='Ackrome',
