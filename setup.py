1382d5faf672dbd3a3349929c9295a6a971ddcdc
Ackrome <ivantishchenko59@gmail.com>
2024-10-31 01:44:35 +0300

    Выпуск версии 0.2.0
    Полностью модернизирована работа модуля tvims

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='matplobblib',
-    version='0.1.40',
+    version='0.2.0',
     packages=find_packages(),
     description='Just a library for some subjects',
     author='Ackrome',
