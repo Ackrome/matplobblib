40d237e696d18dd46d40e9dc45c6de7a9ade2119
Ackrome <ivantishchenko59@gmail.com>
2024-10-30 15:03:14 +0300

    Специальные дискретные случайные величины
    Группа функций преобразована, использование облегчено

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='matplobblib',
-    version='0.1.35',
+    version='0.1.36',
     packages=find_packages(),
     description='Just a library for some subjects',
     author='Ackrome',
