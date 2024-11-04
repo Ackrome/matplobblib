1f9a41a77292baf2be28bfa3978579c6350e8571
Ackrome <ivantishchenko59@gmail.com>
2024-11-04 14:30:28 +0300

    Добавлен раздел Эмперические Характеристики

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='matplobblib',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     description='Just a library for some subjects',
     author='Ackrome',
