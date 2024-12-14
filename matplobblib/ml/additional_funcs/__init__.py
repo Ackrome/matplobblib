94f4927d400305110fdb48464bf445a794f9ad13
Ackrome <ivantishchenko59@gmail.com>
2024-12-14 20:28:17 +0300

    Release 0.2.35 --- new: ml.metrics

--- matplobblib/ml/additional_funcs/__init__.py
+++ matplobblib/ml/additional_funcs/__init__.py
@@ -1,1 +1,2 @@
-from .additional_funcs import *
+from .b2_visual import *
+from .metrics import *
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='matplobblib',
-    version='0.2.34',
+    version='0.2.35',
     packages=find_packages(),
     description='Just a library for some subjects',
     author='Ackrome',
