15cf85509b390c484bb7fd779b5969b66f25d8d7
Ackrome <ivantishchenko59@gmail.com>
2025-02-14 10:53:20 +0300

    new metrics added

--- matplobblib/ml/additional_funcs/metrics/metrics.py
+++ matplobblib/ml/additional_funcs/metrics/metrics.py
@@ -125,6 +125,39 @@ def weighted_average_percentage_error(y_true, y_pred):
     
     return wape
 #########################################################################
+# MSLE
+#########################################################################
+def mean_squared_logarithmic_error(y_true, y_pred, c=1):
+    """
+    Вычисляет среднеквадратичную логарифмическую ошибку (MSLE).
+
+    Args:
+        y_true (iterable): Реальные значения.
+        y_pred (iterable): Предсказанные значения.
+        c (float, optional): Смещение, добавляемое к значениям для избежания логарифма отрицательных чисел. По умолчанию 1.
+
+    Returns:
+        float: среднеквадратичная логарифмическая ошибка.
+
+    Raises:
+        ValueError: Если y_true или y_pred (с учетом c) имеют недопустимые значения.
+    """
+    assert len(y_true) == len(y_pred) > 1, 'Предсказанные и реальные значения должны быть представлены одномерными итерируемыми объектами, содержащими более 1 элемента'
+    
+    if not isinstance(y_true,np.ndarray):
+        y_true = np.array(y_true)
+        
+    if not isinstance(y_pred,np.ndarray):
+        y_pred = np.array(y_pred)
+        
+    if np.any(y_true + c <= 0) or np.any(y_pred + c <= 0):
+        raise ValueError("Значения y_true и y_pred с учетом смещения c должны быть строго положительными")
+
+    
+    rmsle = (1/len(y_true) * np.sum(np.subtract(np.log(y_true+c),np.log(y_pred+c))))
+    
+    return rmsle
+#########################################################################
 # RMSLE
 #########################################################################
 def root_mean_squared_logarithmic_error(y_true, y_pred, c=1):
@@ -157,3 +190,61 @@ def root_mean_squared_logarithmic_error(y_true, y_pred, c=1):
     rmsle = np.sqrt(1/len(y_true) * np.sum(np.subtract(np.log(y_true+c),np.log(y_pred+c))**2))
     
     return rmsle
+#########################################################################
+# MedAE
+#########################################################################
+def median_absolute_error(y_true, y_pred, multioutput = "uniform_average"):
+    """
+    Вычисляет медианную абсолютную ошибку (MedAE).
+
+    Args:
+        y_true (iterable): Реальные значения.
+        y_pred (iterable): Предсказанные значения.
+        multioutput (str, optional): {'raw_values', 'uniform_average'}\
+            Defines aggregating of multiple output values.
+
+    Returns:
+        float: медианная абсолютная ошибка.
+    """
+    assert len(y_true) == len(y_pred) > 1, 'Предсказанные и реальные значения должны быть представлены одномерными итерируемыми объектами, содержащими более 1 элемента'
+    
+    if not isinstance(y_true,np.ndarray):
+        y_true = np.array(y_true)
+        
+    if not isinstance(y_pred,np.ndarray):
+        y_pred = np.array(y_pred)
+    
+    output_errors  = np.median(np.abs(y_pred - y_true), axis=0)
+    
+    if isinstance(multioutput, str):
+        if multioutput == "raw_values":
+            return output_errors
+        elif multioutput == "uniform_average":
+            # pass None as weights to np.average: uniform mean
+            multioutput = None
+
+    return np.average(output_errors, weights=multioutput)
+#########################################################################
+# ME
+#########################################################################
+def max_error(y_true, y_pred):
+    """
+    Вычисляет максимальную ошибку (ME).
+
+    Args:
+        y_true (iterable): Реальные значения.
+        y_pred (iterable): Предсказанные значения.
+
+
+    Returns:
+        float: максимальная ошибка.
+    """
+    assert len(y_true) == len(y_pred) > 1, 'Предсказанные и реальные значения должны быть представлены одномерными итерируемыми объектами, содержащими более 1 элемента'
+    
+    if not isinstance(y_true,np.ndarray):
+        y_true = np.array(y_true)
+        
+    if not isinstance(y_pred,np.ndarray):
+        y_pred = np.array(y_pred)
+
+    return np.max(np.abs(y_pred - y_true))
